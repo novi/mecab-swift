542df7cead396fc1ff8ef6dc259386ac048d77a3
Yusuke Ito <novi.mad@gmail.com>
2015-12-26 00:07:26 +0900

    add Package.swift for `swift build`

---
+++ Package.swift
@@ -0,0 +1,14 @@
+import PackageDescription
+
+#if os(OSX)
+	let CMeCabURL = "https://github.com/novi/CMeCab-OSX.git"
+#else
+	let CMeCabURL = "https://github.com/novi/CMeCab.git"
+#endif
+
+let package = Package(
+    name: "MeCab",
+    dependencies: [
+        .Package(url: CMeCabURL, majorVersion: 1)
+    ]
+)
