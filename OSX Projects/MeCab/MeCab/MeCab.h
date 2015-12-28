0a91a390bef18ebc3fae09f867769f8c64e7e4fa
Yusuke Ito <novi.mad@gmail.com>
2015-12-28 17:37:07 +0900

    import Foundation in entire Framework instead of Cocoa

--- OSX Projects/MeCab/MeCab/MeCab.h
+++ OSX Projects/MeCab/MeCab/MeCab.h
@@ -6,14 +6,12 @@
 //  Copyright © 2015 Yusuke Ito. All rights reserved.
 //
 
-#import <Cocoa/Cocoa.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for MeCab.
 FOUNDATION_EXPORT double MeCabVersionNumber;
 
 //! Project version string for MeCab.
 FOUNDATION_EXPORT const unsigned char MeCabVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <MeCab/PublicHeader.h>
 
 
